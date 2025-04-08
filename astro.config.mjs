8adf705a295a0b900b30eb97347704e86701aee6
Ziel TUF <zielalfinoakbar@gmail.com>
2025-04-09 06:49:51 +0700

    gatau error dimana

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+      plugins: [tailwindcss()],
+    },
 });
